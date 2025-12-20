75bc0f1cf7876a8509c7e85c0bdc5f5d764dbb07
WvanderLaan <116180358+WoutervanderLaan@users.noreply.github.com>
2025-12-20 15:04:58 +0200

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,4 @@ const {withStorybook} = require('@storybook/react-native/metro/withStorybook') /
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname)
 
-module.exports = withStorybook(config) // <-- add this
+module.exports = withStorybook(config)
